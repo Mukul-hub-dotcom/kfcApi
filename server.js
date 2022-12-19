f59bdc55f40f2631fd99af50c2fbe40dc0f853a9
Mukul <65118538+Mukul-hub-dotcom@users.noreply.github.com>
2022-12-19 11:41:25 +0530

    done

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
+const contact=jsonServer.router('contacto.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
+server.use(contact);
 
 server.listen(port);
